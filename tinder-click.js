5e78e342ce934de610eccac19026c0e9a743ef77
shubham agrawal <manovagyanik1@gmail.com>
2017-08-20 14:00:42 +0530

    making likeone recursive, so no need to randomly wait and swipe, also taking screenshot of each swipe and saving in file

--- tinder-click.js
+++ tinder-click.js
@@ -18,25 +18,47 @@ var wd = require("wd"),
             .setImplicitWaitTimeout(300000);
 
        setTimeout(function() {
-           likeNTimes(10);
+           var fileName = getFileName();
+           likeNTimes(10, fileName);
        }, 10000);
 
-    function likeOne(driver){
-        driver
+    function likeOne(fileName, i, n){
+        return driver
             .elementById("com.tinder:id/gamepad_like")
-            .click();
+            .click()
+            .takeScreenshot().then(
+                function(image, err) {
+                    var currentFileName = fileName+'_'+i+'.png';
+                    require('fs').writeFile(currentFileName, image, 'base64', function(err) {
+                        console.log(err);
+                    });
+                    }
+                ).then(function(){
+                    if(i === n){
+                        driver.quitApp();
+                    } else {
+                        likeOne(fileName, i+1, n);
+                    }
+                })
     }
 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function likeNTimes(n) {
-        var j = 2000;
-        for(var i = 1; i<=n; i++){
-            var tout = getRandomInt(j*i/2, j*i);
-            setTimeout(function () {
-                likeOne(driver);
-            }, tout);
-        }
+    function likeNTimes(n, fileName) {
+        driver.takeScreenshot().then(
+                function(image, err) {
+                    var currentFileName = fileName+'_startState.png';
+                    require('fs').writeFile(currentFileName, image, 'base64', function(err) {
+                        console.log(err);
+                    });
+                    }
+        ).then(function(){
+            likeOne(fileName, 1, n);
+        });
+    }
+
+    function getFileName() {
+        return Date.now();
     }
